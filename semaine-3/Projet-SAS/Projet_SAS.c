35deaac2d0e7a7cecba7df6a80ad0ea918a0aa78
safabalo <85119545+safabalo@users.noreply.github.com>
2021-11-23 11:12:08 +0100

    3rd Update

--- semaine-3/Projet-SAS/Projet_SAS.c
+++ semaine-3/Projet-SAS/Projet_SAS.c
@@ -4,42 +4,51 @@
 #define Max 100
 
 typedef struct{
-	char Nom ;
-	char Prenom ;
-	char CIN ;
+	char Nom[12] ;
+	char Prenom[12] ;
+	char CIN[12];
 	float Montant;
 }Bank;
 //***________Declaration Fichier_______***//
-FILE* f=NULL;
+FILE *f;
 //***___________Fonction client_________***//
 void clientn() 
 {
-	int i, n, A;
+	int i, n= 3, A;
 	Bank clientn[Max];
 //***________Fichier Cree_______***//
-	f = fopen("Donnees_clients.txt","w");
+	f = fopen("Donnees_clients.txt","a");
 	
 	if (f==NULL){
 		printf("le fichier n'existe pas \n");
 	}
 	else
-	{
-//***_______Boucle de creation de donnes pour 100 clients______***//
+	{ //***_______Boucle de creation de donnes pour 100 clients______***//
 		n<=Max;
 		for(i=0;i<n;i++)
 		{
 			
 			printf("Donnez les information du clients numero %d : \n", i+1);
 			printf("Donner le Nom : ");
-			scanf("%s",&clientn[i].Nom );
+			scanf("%s",clientn[i].Nom );
+		
 			printf("Donner le Prenom : ");
-			scanf("%s",&clientn[i].Prenom );
+			scanf("%s",clientn[i].Prenom );
+		
 			printf("Donner le CIN : ");
-			scanf("%s",&clientn[i].CIN );
+			scanf("%s",clientn[i].CIN );
+		
 			printf("Donner le Montant: ");
 			scanf("%f",&clientn[i].Montant );
-		}
 		
+		}
+		for(i=0;i<n;i++)
+		{
+	    	fprintf(f ,"Nom: %s \t", clientn[i].Nom);
+			fprintf(f ,"Prenom: %s \t", clientn[i].Prenom);
+			fprintf(f ,"CIN: %s \t", clientn[i].CIN);
+			fprintf(f ,"Montant: %f \t", clientn[i].Montant);
+			}
 	}
 	
 	fclose(f);
@@ -47,29 +56,38 @@ void clientn()
 //***________Fonction Nouveau Client______***//
 void client1()
 {
-	Bank client1[Max];
-	f = fopen("Donnees_clients.txt","w");
+	Bank client1;
+	f = fopen("Donnees_clients.txt","a");
 	if (f==NULL){
 		printf("le fichier n'existe pas \n");
 	}
 	else
-	{
-		system ("cls");
+	{ 	system ("cls");
 		printf("Donnez les information de nouveau client : \n");
+			
 			printf("Donner le Nom : ");
-			scanf("%s",&client1[12].Nom );
+			scanf("%s",client1.Nom );
 			printf("Donner le Prenom : ");
-			scanf("%s",&client1[12].Prenom );
+			scanf("%s",client1.Prenom );
 			printf("Donner le CIN : ");
-			scanf("%s",&client1[12].CIN );
+			scanf("%s",client1.CIN );
 			printf("Donner le Montant: ");
-			scanf("%f",&client1[12].Montant ); }
+			scanf("%f",&client1.Montant );
+			fprintf(f ,"Nom: %s \t", client1.Nom);
+			
+			fprintf(f ,"Prenom: %s \t", client1.Prenom);
+			
+			fprintf(f ,"CIN: %s \t", client1.CIN);
+			
+			fprintf(f ,"Montant: %f \t", client1.Montant); }
 	fclose(f);
 }
+
+
 //********_____________Menu_____________**********//
 void menu()
 { int Menu_Bank_X;
-	printf("*****Menu Bank X*********\n");
+	printf("********Menu Bank X********* \n");
 	printf("1- Nouveau clients \n");
 	printf("2- Ajouter plusieurs clients \n");
 	printf("3- Affichage \n");
@@ -78,16 +96,23 @@ void menu()
 	scanf("%d",&Menu_Bank_X);
 	switch (Menu_Bank_X)
 		{
-			case 1: printf("Nouveau client \n");
-					break;
-			case 2: printf("Ajouter plusieurs clients \n");
-					break;
-			case 3: printf("Affichage \n");
-					break;
-			case 4: printf("Quitter \n") ;
-					break;
-			default: printf("Eureur de saisie \n");
-					break;		
+			case 1: 
+				printf("Nouveau client \n");
+				client1();
+				break;
+			case 2: 
+				printf("Ajouter plusieurs clients \n");
+				clientn();
+				break;
+			case 3: 
+				printf("Affichage \n");
+				break;
+			case 4: 
+				printf("Quitter \n") ;
+				break;
+			default: 
+				printf("Eureur de saisie \n");
+				break;		
 		}
 }
 //***fonction main______***//
@@ -97,5 +122,3 @@ int main()
 	
 	return 0;
 }
-
-
